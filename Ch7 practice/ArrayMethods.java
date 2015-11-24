d72e3ed5e54357f2c120cfeb557d878f354da7df
hyan1 <hyan@NN123-HA-15.HS.sd203.org>
2015-11-24 15:06:40 -0600

    11/24

---
+++ Ch7 practice/ArrayMethods.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class ArrayMethods
+{ 
+    private int[] values;
+ 
+    public ArrayMethods(int[] initialValues) 
+    { 
+        values = initialValues; 
+    }
+    
+    public void swapFirstAndLast() 
+    {  
+        ...
+    }
+    
+    public void shiftRight() 
+    { 
+        ... 
+    }
+    
+    public void replace0()
+    {
+        if( 
+    }
+}
